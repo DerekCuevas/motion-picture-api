da1086459404a84b7820d0a1a7ebac59c15c5ffa
Derek Cuevas <dccuevas2@gmail.com>
2015-10-28 14:20:48 -0700

    added eslint setup, working on ideas

--- Inventory.js
+++ Inventory.js
@@ -1,11 +1,29 @@
-'use strict';
+import _ from 'lodash';
 
-const _ = require('lodash');
-
-class Inventory {
+export default class Inventory {
     constructor() {
         this.movies = [];
+        this.queryCache = {};
+    }
+
+    save() {
+    }
+
+    filter(genres = [], category = '', fuzzy = '') {
+    }
+
+    pageinate(set, limit, offset) {
+    }
+
+    total() {
+    }
+
+    search(id, offset = 0, limit = 10, genres, category, fuzzy) {
+    }
+
+    getMovie(sku) {
+    }
+
+    setMovie() {
     }
 }
-
-module.exports = Inventory;
