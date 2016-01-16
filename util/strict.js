00bee3fa4e30ae8088523c39902ab3187d7b37db
Derek Cuevas <dccuevas2@gmail.com>
2016-01-16 13:00:38 -1000

    updated search function

--- util/strict.js
+++ util/strict.js
@@ -1,13 +1,7 @@
-import {some} from 'lodash';
-
 export default function strict(str, pattern) {
-    const keywords = pattern.split(' ');
-
     if (!str || !pattern) {
         return false;
     }
 
-    return some(keywords, word => {
-        return str.toString().toLowerCase().search(word.toLowerCase()) !== -1;
-    });
+    return str.toString().toLowerCase().search(pattern.toLowerCase()) !== -1;
 }
