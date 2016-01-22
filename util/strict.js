11601289f6ef3c7c3646c3d40edf3fa5a7d7cf3d
Derek Cuevas <dccuevas2@gmail.com>
2016-01-22 15:57:30 -0800

    refactored selected movie state, updated tests, and UI

--- util/strict.js
+++ util/strict.js
@@ -3,5 +3,8 @@ export default function strict(str, pattern) {
         return false;
     }
 
+    // FIXME: '\' throws
+
+    // FIXME: use .includes here
     return str.toString().toLowerCase().search(pattern.toLowerCase()) !== -1;
 }
